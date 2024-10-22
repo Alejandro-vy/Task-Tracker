ca30ad007a2c8b2d128c979f3dab998f8b9ef91c
Alejandro_v <a.vera07@ufromail.cl>
2024-10-22 00:02:13 -0300

    Agregamos metodos para manejar archivos

--- src/main/java/avera/Task_Tracker/repository/TaskRepository.java
+++ src/main/java/avera/Task_Tracker/repository/TaskRepository.java
@@ -1,7 +1,87 @@
 package avera.Task_Tracker.repository;
 
+import avera.Task_Tracker.model.Task;
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class TaskRepository {
+
+    private final Path filePath = Paths.get("tasks.json");
+
+    // Inicializa el archivo JSON si no existe
+    public TaskRepository() {
+        if (!Files.exists(filePath)) {
+            createEmptyJson();
+        }
+    }
+
+    // Lee las tareas del archivo JSON
+    public List<Task> getTasks() {
+        try {
+            String content = Files.readString(filePath);
+            // Parsear el contenido JSON y devolver la lista de tareas
+            return parseJson(content);
+        } catch (IOException e) {
+            System.out.println("Error al leer o procesar el archivo JSON: " + e.getMessage());
+        }
+        return null;
+    }
+
+    // Escribe la lista de tareas en el archivo JSON
+    public void saveTasks(List<Task> tasks) {
+        try {
+            String jsonContent = convertToJson(tasks);
+            Files.writeString(filePath, jsonContent);
+        } catch (IOException e) {
+            // Manejar errores
+        }
+    }
+
+    private void createEmptyJson() {
+        try {
+            Files.writeString(filePath, "[]");  // Archivo JSON vacío
+        } catch (IOException e) {
+            // Manejar errores
+        }
+    }
+
+    public List<Task> parseJson (String filePath) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String jsonString = new String(Files.readAllBytes(Paths.get(filePath)));
+
+        List<Task> tasks = objectMapper.readValue(jsonString, new TypeReference<List<Task>>() {
+        });
+
+
+        return tasks;
+
+    }
+
+    public String convertToJson (List<Task> tasks) throws IOException{
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String jsonStr = objectMapper.writeValueAsString(tasks);
+
+        return jsonStr;
+
+    }
+
+
 
 
 
 }
+
+
+
+
